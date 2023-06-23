c3880ae9155bd8d079e78efaf3e70b4ad996a2af
DESKTOP-K33JG8F\202-00000 <dygks0602@naver.com>
2023-06-23 10:37:03 +0900

    반복문 추가

--- 02_stock_price.py
+++ 02_stock_price.py
@@ -1,12 +1,17 @@
 import requests # 정보 달라고 웹에 요청
 from bs4 import BeautifulSoup # 가져온 정보를 parsing 하기
 # 주식 가격 가져오기
+'''
+038880, 005930, 086520
+'''
+finance_list = [('038880', '아이에이'), ('005930', "삼성전자"), ('086520', "에코프로")]
+for i in finance_list :
+    url = f"https://finance.naver.com/item/sise.naver?code={i[0]}"
+    response = requests.get(url)
+    html = response.text
 
-url = "https://finance.naver.com/item/sise.naver?code=005930"
-response = requests.get(url)
-html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
 
-soup = BeautifulSoup(html, 'html.parser')
-
-price = soup.select_one("strong#_nowVal.tah.p11")
-print(price.text)
+    price = soup.select_one("strong#_nowVal.tah.p11")
+    print(i[1] + " 의 가격은 : ", end="$")
+    print(price.text)
